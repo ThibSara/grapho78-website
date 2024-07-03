cf8531b3efc6e8aaacffc036f917a61019708427
ThibSara <sara.thibierge@gmail.com>
2024-07-03 11:20:51 +0200

    refactor: Update image layout and add key prop in ContactSection

--- src/app/sections/prendre-rdv/ContactSection.tsx
+++ src/app/sections/prendre-rdv/ContactSection.tsx
@@ -95,30 +95,24 @@ export const ContactSection = () => {
           </div>
           <div className="lg:pr-4 flex justify-center lg:justify-end">
             <div className="lg:flex-1 lg:mt-8 lg:self-start items-center">
-              <Suspense
-                fallback={
-                  <Skeleton className="w-[200px] h-[200px] rounded-2xl bg-slate-200" />
-                }
-              >
+              <img
+                src="/images/sandrine-thibierge.png"
+                alt=""
+                className="aspect-[7/5] w-[20rem] mt-8 lg:mt-0 lg:w-[24rem] max-w-none rounded-2xl bg-gray-50 object-cover"
+              />
+              <div className="flex">
                 <img
-                  src="/images/sandrine-thibierge.png"
+                  src="/images/seances/2.jpg"
                   alt=""
-                  className="aspect-[7/5] w-[20rem] mt-8 lg:mt-0 lg:w-[24rem] max-w-none rounded-2xl bg-gray-50 object-cover"
+                  className="aspect-[4/3] w-[12rem]  lg:w-[16rem] mt-5  max-w-none flex-none rounded-2xl bg-gray-50 object-cover hidden sm:block"
                 />
-                <div className="flex">
-                  <img
-                    src="/images/seances/2.jpg"
-                    alt=""
-                    className="aspect-[4/3] w-[12rem]  lg:w-[16rem] mt-5  max-w-none flex-none rounded-2xl bg-gray-50 object-cover hidden sm:block"
-                  />
 
-                  <img
-                    src="/images/seances/7.jpg"
-                    alt=""
-                    className="aspect-[4/3] w-[20rem] lg:w-[16rem] mt-5  sm:ml-5 ml-0 max-w-none flex-none rounded-2xl bg-gray-50 object-cover"
-                  />
-                </div>
-              </Suspense>
+                <img
+                  src="/images/seances/7.jpg"
+                  alt=""
+                  className="aspect-[4/3] w-[20rem] lg:w-[16rem] mt-5  sm:ml-5 ml-0 max-w-none flex-none rounded-2xl bg-gray-50 object-cover"
+                />
+              </div>
             </div>
           </div>
         </div>
